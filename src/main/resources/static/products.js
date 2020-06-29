8174af638820dcc019d5f8d5b83d44ecb8ce84ab
Roberto Marquez <onebeartoe@gmail.com>
2020-06-28 22:02:24 -0500

    dynamically load the product listing

---
+++ src/main/resources/static/products.js
@@ -0,0 +1,40 @@
+
+function logEvent(message, divId)
+{
+    var e = document.getElementById(divId);
+    
+    var logs = message + "<br/>" + e.innerHTML;
+    
+    e.innerHTML = logs;
+}
+
+function refreshProducts()
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            logEvent("products are loaded", "logs");
+          
+            var productArray = JSON.parse(this.responseText);
+            
+            var productHtml = "";
+            
+            productArray.forEach(element => 
+            {
+                productHtml += element.name + ":" + element.role;
+                
+                productHtml += "<br>"
+            });
+            
+            
+            
+            var e = document.getElementById("products");
+            
+            e.innerHTML = productHtml;
+        }
+    };
+    xmlhttp.open("GET", "/employees", true);
+    xmlhttp.send();    
+}
