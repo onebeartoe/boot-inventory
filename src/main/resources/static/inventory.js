602783846283b71ffacb38244117f6707d56d814
Roberto Marquez <onebeartoe@gmail.com>
2020-06-29 09:51:23 -0500

    add an inventory admin page

---
+++ src/main/resources/static/inventory.js
@@ -0,0 +1,34 @@
+
+function refreshInventory()
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            logEvent("products are loaded", "logs");
+          
+            var productArray = JSON.parse(this.responseText);
+            
+            var productHtml = "";
+            
+            productArray.forEach(element => 
+            {
+                productHtml += element.name + ":" + element.role;
+                
+                productHtml += "<br>"
+            });
+                    
+            var e = document.getElementById("inventory");
+            
+            e.innerHTML = productHtml;
+        }
+    };
+    xmlhttp.open("GET", "/api/inventory", true);
+    xmlhttp.send();    
+}
+
+function submitNewProduct()
+{
+    
+}
